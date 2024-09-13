fb6b7980faf9150085541b9d1105c15a274a3e93
Ahsan280 <ahsanrehman281@gmail.com>
2024-09-13 10:47:40 +0500

    Employee Attendance DatePicker 4

--- src/components/MyDatePicker.jsx
+++ src/components/MyDatePicker.jsx
@@ -3,10 +3,11 @@ import { DatePicker, Space } from "antd";
 
 const MyDatePicker = ({ setDate }) => {
   const onChange = (date, dateString) => {
-    setDate(() => {
-      console.log(new Date(date));
-      return new Date(date).toISOString();
-    });
+    const selectedDate = new Date(date);
+    selectedDate.setHours(selectedDate.getHours() + 5);
+    const isoString = selectedDate.toISOString();
+    console.log(isoString);
+    setDate(isoString);
   };
   return (
     <Space direction="vertical">
