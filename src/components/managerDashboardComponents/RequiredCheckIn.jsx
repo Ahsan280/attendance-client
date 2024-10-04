a0f78d3aa402ff7bdb4d16622b006a1e719d9fb7
Ahsan280 <ahsanrehman281@gmail.com>
2024-10-04 12:15:24 +0500

    get shift

--- src/components/managerDashboardComponents/RequiredCheckIn.jsx
+++ src/components/managerDashboardComponents/RequiredCheckIn.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from "react";
 import useAxios from "../../utils/useAxios";
 import ChangeTimeModal from "./ChangeTimeModal";
+import { useAuthContext } from "../../context/AuthContext";
 
 function RequiredCheckIn() {
   const api = useAxios();
-
+  const { user } = useAuthContext();
   const [time, setTime] = useState("");
+  const [startTime, setstartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
   useEffect(() => {
     const fetchRequiredCheckInTime = async () => {
-      const response = await api.get(`v1/time/get-time`);
+      const response = await api.get(
+        `v1/shift/get-shift/${user._id ? user._id : user.id}`
+      );
+      setstartTime(response.data.shift.startTime);
+      setEndTime(response.data.shift.endTime);
       setTime(response.data.time.requiredCheckInTime);
     };
     fetchRequiredCheckInTime();
   }, []);
   return (
     <div className="d-flex gap-3">
-      <h2>Required Check In Time: {time}</h2>
-      <ChangeTimeModal time={time} setTime={setTime} />
+      <h2>
+        Required Check In Time: From:{startTime} To:{endTime}
+      </h2>
+      {/* <ChangeTimeModal time={time} setTime={setTime} /> */}
     </div>
   );
 }
